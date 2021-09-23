036b36ad862a573bb242eabfb89c990dc4755bd7
JJ <jc@orbsdigital.com>
2021-09-23 18:11:47 +1000

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+/*inital JavaScript- adding basic code */
+const h2 = document.createElement('h2');
+h2.textContent = "This content added by JavaScript";
+
+/*add h2 element to the body element in the DOM.*/
+document.querySelector('body').appendChild(h2);
