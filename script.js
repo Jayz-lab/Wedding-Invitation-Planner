3449eead75f70091eed09704a4849f23e40875ed
JJ <jc@orbsdigital.com>
2021-09-24 16:39:55 +1000

    Completed intial html structure & css styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /*inital JavaScript- adding basic code: creating h2 element w/txtContent */
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+//const h2 = document.createElement('h2');
+//h2.textContent = "This content added by JavaScript";
 
 /*add h2 element to the body element in the DOM.*/
-document.querySelector('body').appendChild(h2);
+//document.querySelector('body').appendChild(h2);
+
+//onClikc Event: https://www.w3schools.com/jsref/event_onclick.asp
