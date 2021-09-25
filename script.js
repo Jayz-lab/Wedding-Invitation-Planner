1343fd783ec56137edda3283f09f4e715041e6b7
JJ <jc@orbsdigital.com>
2021-09-25 19:08:57 +1000

    Adding design options

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 /*add h2 element to the body element in the DOM.*/
 //document.querySelector('body').appendChild(h2);
 
-//onClikc Event: https://www.w3schools.com/jsref/event_onclick.asp
 //Design choices- 3 different colors
 /* color change
 const elemID = document.getElementById('right-col');
@@ -32,8 +31,5 @@ function design3(){
 }
 
 //live text
-function getName() {
-    console.log("I am activated");
-    var nameStr = document.getElementById('name').value;
-    document.getElementById("initial").innerHTML = nameStr.substr(0,1).toUpperCase();
-}
+//const elemIDFont = document.getElementById('msg-area');
+
