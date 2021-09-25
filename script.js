eb35f3b74766808f3e8ae30f18dffc43efd93fb7
JJ <jc@orbsdigital.com>
2021-09-25 15:49:35 +1000

    Adding changes to button choices to image

--- script.js
+++ script.js
@@ -6,3 +6,34 @@
 //document.querySelector('body').appendChild(h2);
 
 //onClikc Event: https://www.w3schools.com/jsref/event_onclick.asp
+//Design choices- 3 different colors
+/* color change
+const elemID = document.getElementById('right-col');
+
+function changeBackground(color){
+    elemID.style.backgroundColor = color;
+}
+*/
+
+//image change- 3 choices- 3 functions
+function design1(){
+    document.getElementById('myImage')
+    .src="/image/image1.jpg";
+}
+
+function design2(){
+    document.getElementById('myImage')
+    .src="/image/image2.jpg";
+}
+
+function design3(){
+    document.getElementById('myImage')
+    .src="/image/image3.jpg";
+}
+
+//live text
+function getName() {
+    console.log("I am activated");
+    var nameStr = document.getElementById('name').value;
+    document.getElementById("initial").innerHTML = nameStr.substr(0,1).toUpperCase();
+}
